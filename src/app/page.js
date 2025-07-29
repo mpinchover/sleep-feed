868c141e6dc474e2731a78638997bad3238597ef
mpinchover <mpinchover@gmail.com>
2025-07-28 18:58:28 -0700

    got scroll to work

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { Box, Flex } from "@chakra-ui/react";
+import initial_images from "./fake-video-cards";
 
-export default function Home() {
+const VideoCard = ({ src }) => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      border="solid 1px yellow"
+      height="100vh"
+      // border="solid 1px green"
+      scrollSnapAlign="start"
+      scrollSnapStop="always"
+      position="relative"
+      // sx={{ scrollSnapAlign: "start" }}
+    >
+      <Box
+        borderRadius="10px"
+        border="solid 1px blue"
+        width="350px"
+        overflow="hidden"
+      >
+        <video
+          style={{ objectFit: "cover", display: "block" }}
+          src={src}
+          height="100%"
+          width="100%"
+          autoPlay
+          muted
+          loop
+          playsInline
+          onError={(e) => console.error("Video failed to load", e)}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </Box>
+    </Flex>
   );
-}
+};
+
+const Home = () => {
+  return (
+    <Box
+      // direction="column"
+      // alignItems="center"
+      overflowY="scroll"
+      // minHeight="100vh"
+      scrollSnapType="y mandatory"
+      overscrollBehavior={"contain"}
+      // scrollSnapPointsY="repeat(100vh)"
+      // scrollSnapMarginY="10"
+      height="100vh"
+      // sx={{
+      //   scrollSnapType: "y mandatory",
+      //   scrollBehavior: "smooth",
+      // }}
+    >
+      {initial_images.map((video, index) => (
+        <VideoCard key={index} src={video.src} />
+      ))}
+    </Box>
+  );
+};
+
+export default Home;
